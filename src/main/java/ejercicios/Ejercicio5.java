87f005d9c9fbfc8a9b9a88082f8807b97f404365
carlos.iglesiasgomez <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-03-17 20:50:20 +0100

    inicio del ejercicio 5

---
+++ src/main/java/ejercicios/Ejercicio5.java
@@ -0,0 +1,22 @@
+package ejercicios;
+
+/**
+ * Realizar una clase Primitiva que tenga definido un Array privado de 6
+ * elementos con el resultado de un sorteo de la primitiva (serán 6 enteros con
+ * valores comprendidos entre 1 y 49 y sin repetidos). La clase dispondrá de un
+ * constructor en el que se generan y almacenen esos números al azar, también
+ * tendrá un método al que se le pase una combinación jugada como parámetro (no
+ * necesariamente ordenada) y devuelva el número de aciertos. Realiza tres
+ * programas distintos que usen esa clase: Programa que el usuario introduzca
+ * los 6 números de su boleto y le diga cuantos acertó. Programa en el que se
+ * generen 1000 boletos al azar y nos informe de cuantos boletos hay con 3, 4, 5
+ * y 6 aciertos (a lo mejor es necesario crear un nuevo método en la clase
+ * Primitiva para generar boletos al azar). Programa con un bucle que genere
+ * boletos hasta que acierte los 6 números ¿Cuántos boletos has tenido que
+ * crear?
+ * 
+ */
+
+public class Ejercicio5 {
+    
+}
