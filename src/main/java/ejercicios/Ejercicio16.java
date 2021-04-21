b82b90676eed70d7485c3f03b5257ae026bb7257
carlos.iglesiasgomez <carlos.iglesiasgomez@wirtz.local>
2021-04-21 17:37:29 +0200

    15 listo

---
+++ src/main/java/ejercicios/Ejercicio16.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+/**
+ * 7.16. Realizar un programa al que se le vayan introduciendo por teclado
+ * números enteros. El programa dispone de dos ArrayList, uno llamado positivos
+ * y otro negativos. Se trata de meter los números introducidos en uno u otro
+ * según su signo hasta que finalice el programa (esto ocurrirá cuando se
+ * introduzca cero). Al finalizar, mostrará la media aritmética de cada
+ * ArrayList.
+ */
+
+public class Ejercicio16 {
+
+}
