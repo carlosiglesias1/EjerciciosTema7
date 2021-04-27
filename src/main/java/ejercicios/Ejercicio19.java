480c994cc2606bfc7afa4a738942130cb7a727f7
carlos.iglesiasgomez <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-04-27 21:38:34 +0200

    DIIOS

---
+++ src/main/java/ejercicios/Ejercicio19.java
@@ -0,0 +1,19 @@
+package ejercicios;
+
+/**
+ * 7.19. Realizar una clase carritoCompra2 que mantenga las compras realizadas
+ * por un cliente en un ArrayList. Cada posición del ArrayList contendrá el
+ * código del producto, la descripción, el precio unitario y la cantidad de
+ * unidades compradas y el precio total de ese producto (cantidad x precio
+ * unitario). Se permite comprar un número indeterminado de artículos. Además de
+ * la lista de productos, la clase tiene el importe total de la compra, que debe
+ * estar siempre actualizado. Necesitamos los métodos: a) Mostrar por consola el
+ * estado actual del carrito. b) Vaciar carrito. c) Añadir producto. d) Eliminar
+ * producto (se le pasa el código de producto) y lo elimina físicamente del
+ * ArrayList. Hacer un programa con un menú que permita al usuario operar con el
+ * carrito de la compra.
+ */
+
+public class Ejercicio19 {
+
+}
