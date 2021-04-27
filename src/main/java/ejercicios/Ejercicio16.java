832d30cc91a625276e5f3bcb13f2aa6d1a9e9f4c
carlos.iglesiasgomez <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-04-27 19:59:16 +0200

    commit pa pullear

--- src/main/java/ejercicios/Ejercicio16.java
+++ src/main/java/ejercicios/Ejercicio16.java
@@ -1,5 +1,8 @@
 package ejercicios;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * 7.16. Realizar un programa al que se le vayan introduciendo por teclado
  * números enteros. El programa dispone de dos ArrayList, uno llamado positivos
@@ -10,5 +13,28 @@ package ejercicios;
  */
 
 public class Ejercicio16 {
+    static float average(ArrayList<Integer> list) {
+        float average = 0;
+        for (int i = 0; i < list.size(); i++) {
+            average += list.get(i);
+        }
+        return average / list.size();
+    }
 
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int aux;
+        ArrayList<Integer> positiveList = new ArrayList<Integer>();
+        ArrayList<Integer> negativeList = new ArrayList<Integer>();
+        do {
+            aux = teclado.nextInt();
+            if (aux > 0)
+                positiveList.add(aux);
+            if (aux < 0)
+                negativeList.add(aux);
+        } while (aux != 0);
+        System.out.println("La media de los numeros positivos es: " + average(positiveList));
+        System.out.println("La media de los números negativos es: " + average(negativeList));
+        teclado.close();
+    }
 }
---
+++ src/main/java/ejercicios/Ejercicio17.java
@@ -0,0 +1,20 @@
+package ejercicios;
+
+/**
+ * 7.17. Realiza una clase llamada Primitiva2 similar a la clase Primitiva, pero
+ * empleando ahora una ArrayList, y aprovechando los métodos de los que dispone
+ * para simplificar la generación de números sin repetidos. Además, los números
+ * premiados se mantendrán ordenados y, por último, la búsqueda de los números
+ * jugados en el ArrayList de los premiados se requiere que se haga de forma
+ * dicotómica. Rehacer los 3 programas del ejercicio anterior de la Primitiva
+ * pero usando esta nueva clase.  Programa que el usuario introduzca los 6
+ * números de su boleto y le diga cuantos acertó.  Programa en el que se
+ * generen 1000 boletos al azar y nos informe de cuantos boletos hay con 3, 4, 5
+ * y 6 aciertos (a lo mejor es necesario crear un nuevo método en la clase
+ * Primitiva para generar boletos al azar).  Programa con un bucle que genere
+ * boletos hasta que acierte los 6 números ¿Cuántos boletos has tenido que
+ * crear?
+ */
+public class Ejercicio17 {
+
+}
