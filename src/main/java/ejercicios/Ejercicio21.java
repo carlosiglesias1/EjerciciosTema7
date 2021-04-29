13e494544487e56c0d8b26e0d94f52863130ddf1
carlos.iglesiasgomez <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-04-29 20:21:10 +0200

    ale ale

---
+++ src/main/java/ejercicios/Ejercicio21.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+/**
+ * 7.21. Realizar una versión del juego de cartas de las 7 y media para 3
+ * jugadores. Los jugadores van solicitando cartas 1 a 1 para llegar a las 7 y
+ * media sin pasarse, pudiendo plantarse cuando lo desee. Las figuras valen
+ * medio punto y el resto su valor numérico. La baraja tiene 40 cartas. El juego
+ * se acaba cuando todos los jugadores se planten, se hayan pasado, o se acaben
+ * las cartas.
+ */
+
+public class Ejercicio21 {
+
+}
