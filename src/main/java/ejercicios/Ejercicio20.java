5368ca6efc24a1eb50efacaeb718ae8b4e2ce944
carlos.iglesiasgomez <carlos.iglesiasgomez@WIRTZ.LOCAL>
2021-04-29 20:19:30 +0200

    facturas acabadas

--- src/main/java/ejercicios/Ejercicio20.java
+++ src/main/java/ejercicios/Ejercicio20.java
@@ -1,5 +1,9 @@
 package ejercicios;
 
+import java.util.Scanner;
+
+import recursos.facturas.Factura;
+
 /**
  * 7.20. Diseñar una clase Factura que consta de:  Número identificador: lo
  * proporciona el usuario en el alta de la factura.  Fecha de la factura: la
@@ -21,5 +25,56 @@ package ejercicios;
  */
 
 public class Ejercicio20 {
+    static void promptFactura() {
+        System.out.println(
+                "Pulsa 'a' para añadir líneas a la factura\nPulsa 'b' para borrar una línea\nPulsa 's' para salir");
+    }
 
+    static void printLines(Factura factura) {
+        for (int i = 0; i < factura.getLineasFactura().size(); i++) {
+            System.out.println(i + ": " + factura.getLineasFactura().get(i).toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        Factura factura = new Factura("08647", "06789");
+        Scanner teclado = new Scanner(System.in);
+        char opcion;
+        do {
+            promptFactura();
+            opcion = teclado.nextLine().charAt(0);
+            switch (opcion) {
+            case 'a':
+                System.out.println("Introduce una descripcion del producto: ");
+                String description = teclado.nextLine();
+                System.out.println("Introduce el precio: ");
+                float precio = Float.parseFloat(teclado.nextLine());
+                System.out.println("Introduce las unidades: ");
+                int unidades = Integer.parseInt(teclado.nextLine());
+                if (factura.addLine(description, precio, unidades))
+                    printLines(factura);
+                else
+                    System.out.println("La línea no se ha podido añadir");
+                break;
+            case 'b':
+                System.out.println("Qué posición vas a eliminar?");
+                printLines(factura);
+                int position = Integer.parseInt(teclado.nextLine());
+                if (factura.rmLine(position))
+                    printLines(factura);
+                else
+                    System.out.println("La línea no se ha podido borrar");
+                break;
+            case 's':
+                float total = factura.getImporteFactura() + factura.getImporteFactura() * 0.21f;
+                System.out.printf("El precio de esta factura es de %.2f€ + el 21%% de iva (%.2f€) = %.2f€",
+                        factura.getImporteFactura(), factura.getImporteFactura() * 0.21f, total);
+                break;
+            default:
+                System.out.println("Algo no ha ido como debía");
+                break;
+            }
+        } while (opcion != 's');
+        teclado.close();
+    }
 }
