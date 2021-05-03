64f7f7bcae8c7592b6aa4cb4b93821656197b190
carlos <carlosig490@gmail.com>
2021-05-03 21:41:09 +0200

    vamos con el 25

--- src/main/java/ejercicios/Ejercicio25.java
+++ src/main/java/ejercicios/Ejercicio25.java
@@ -1,5 +1,8 @@
 package ejercicios;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * 7.25. Realizar una programa con un ArrayList bidimensional llamado
  * edadesAlumnos en el que cada fila representa cada aula y cada columna los
@@ -11,5 +14,32 @@ package ejercicios;
  */
 
 public class Ejercicio25 {
+    
+    static void printMulti(ArrayList <ArrayList<Integer>> mList){
+        for (int i = 0; i < mList.size(); i++) {
+            System.out.println("Clase : "+(i+1));
+            for (int j = 0; j < mList.get(i).size(); j++) {
+                System.out.print("["+mList.get(i).get(j)+"] ");
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> edadesAlumnos = new ArrayList<ArrayList<Integer>>();
+        int edad;
+        int count = 0;
+        Scanner teclado = new Scanner(System.in);
+        edadesAlumnos.add(new ArrayList<Integer>());
+        do {
+            edad = teclado.nextInt();
+            if (edad == 0) {
+                edadesAlumnos.add(new ArrayList<Integer>());
+                count++;
+            } else
+                edadesAlumnos.get(count).add(edad);
+        } while (edad != -1);
+        printMulti(edadesAlumnos);
+        teclado.close();
+    }
 }
